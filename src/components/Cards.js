2be737009f2880cdf384ce1d5bda954cb18734b7
Mahendra Bhama <40362898+maahibhama@users.noreply.github.com>
2021-04-03 16:21:10 +0530

    fixed some css issues

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,43 +1,35 @@
-import React from 'react';
-import CardItem from './CardItem';
-import './Cards.css';
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
+import image1 from "../images/img-1.jpg";
+import image2 from "../images/img-2.jpg";
+import image3 from "../images/img-home.jpg";
 function Cards() {
-	return (
-		<div className='cards'>
-			<h1>Check out these EPIC Destinations!</h1>
-			<div className='cards__container'>
-				<div className='cards__wrapper'>
-					<ul className='cards__items'>
-						<CardItem
-							//Link of image to be added
-							src='/images/img-1.jpg'
-							text="Explore superfast wash services at your doorstep!"
-							label='Superfast'
-							path='/services'
-						/>
-					</ul>
-					<ul className='cards__items'>
-						<CardItem
-							//Link of image to be added
-							src='/images/img-1.jpg'
-							text="Get a deep clean wash for your car"
-							label='Deep Clean'
-							path='/services'
-						/>
-					</ul>
-					<ul className='cards__items'>
-						<CardItem
-							//Link of image to be added
-							src='/images/img-1.jpg'
-							text="Get the dust out of interior and feather covers of seats"
-							label='Vaccume clean'
-							path='/services'
-						/>
-					</ul>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="cards">
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className="cards__container">
+        <CardItem
+          src={image1}
+          text="Explore superfast wash services at your doorstep!"
+          label="Superfast"
+          path="/services"
+        />
+        <CardItem
+          src={image2}
+          text="Get a deep clean wash for your car"
+          label="Deep Clean"
+          path="/services"
+        />
+        <CardItem
+          src={image3}
+          text={"Get the dust out of interior and feather covers of seats"}
+          label={"Vaccume clean"}
+          path={"/services"}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
